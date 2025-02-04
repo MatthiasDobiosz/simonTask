5c5910e755cea65b701f955baaa8f4974008afd9
Andreas Schmid <andreas.schmid@ur.de>
2025-02-04 17:42:33 +0100

    p6

--- simonTask/Game.h
+++ simonTask/Game.h
@@ -46,7 +46,7 @@ private:
 	bool startRealScreen = false;
 
 	int experimentalCondition = 1;
-	int participantId = 5;
+	int participantId = 7;
 	int latency = 80;
 
 	// interval for saving mouse data 
